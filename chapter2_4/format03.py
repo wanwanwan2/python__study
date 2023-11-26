d0204cbcb899a4a29165ab1de8f058c9bb5e689f
WanPatch <121847253+wanwanwan2@users.noreply.github.com>
2023-11-27 00:38:06 +0900

    숫자와 문자열의 다양한 기능

---
+++ chapter2_4/format03.py
@@ -0,0 +1,13 @@
+#기호 붙여 출력하기
+
+#기호와 함께 출력하기
+output_f = "{:+d}".format(52)
+output_g = "{:+d}".format(-52)
+output_h = "{: d}".format(52)
+output_i = "{: d}".format(-52)
+
+print("# 기호와 함께 출력하기")
+print(output_f)
+print(output_g)
+print(output_h)
+print(output_i)
