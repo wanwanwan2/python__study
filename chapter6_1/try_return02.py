4f81d17d6934da55c4433740832b03248c6445db
WanPatch <121847253+wanwanwan2@users.noreply.github.com>
2023-12-05 12:57:36 +0900

    구문 오류와 예외

---
+++ chapter6_1/try_return02.py
@@ -0,0 +1,20 @@
+# finally 키워드 활용
+
+# 함수를 선언합니다.
+def write_text_file(filename, text):
+    # try except 구문을 사용합니다.
+    try:
+        # 파일을 엽니다.
+        file = open(filename, "w")
+        # 여러 가지 처리를 수행합니다.
+        return
+        # 파일에 텍스트를 입력합니다.
+        file.write(text)
+    except:
+        print("오류가 발생했습니다.")
+    finally:
+        # 파일을 닫습니다.
+        file.close()
+
+# 함수를 호출합니다.
+write_text_file("test.txt", "안녕하세요!")
